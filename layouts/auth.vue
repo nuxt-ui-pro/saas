51629b71e0154e9f6dd7db35b272340ca7d10323
Benjamin Canac <canacb1@gmail.com>
2024-01-05 14:24:17 +0100

    chore(auth): improve home button

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -10,7 +10,7 @@ useHead({
   <div class="h-screen flex items-center justify-center overlay">
     <div class="gradient" />
 
-    <UButton icon="i-heroicons-home" label="Home" to="/" color="white" variant="ghost" class="absolute top-4" />
+    <UButton icon="i-heroicons-home" label="Home" to="/" color="black" class="absolute top-4" />
 
     <slot />
   </div>
