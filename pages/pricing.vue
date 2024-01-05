614450bada111d2646d34e8d33406f960a215ff7
Benjamin Canac <canacb1@gmail.com>
2024-01-05 14:27:53 +0100

    chore(pricing): improve page

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -21,27 +21,27 @@ const isYearly = ref(false)
 </script>
 
 <template>
-  <UContainer v-if="page">
+  <div v-if="page">
     <UPageHero v-bind="page.hero">
       <template #links>
         <UPricingToggle v-model="isYearly" class="w-48" />
       </template>
     </UPageHero>
 
-    <UPage>
-      <UPageBody>
-        <UPricingGrid>
-          <UPricingCard v-for="(plan, index) in page.plans" :key="index" v-bind="plan" :price="isYearly ? plan.price.year : plan.price.month" :cycle="isYearly ? '/year' : '/month'" />
-        </UPricingGrid>
+    <UContainer>
+      <UPricingGrid>
+        <UPricingCard v-for="(plan, index) in page.plans" :key="index" v-bind="plan" :price="isYearly ? plan.price.year : plan.price.month" :cycle="isYearly ? '/year' : '/month'" />
+      </UPricingGrid>
+    </UContainer>
 
-        <ULandingLogos class="mt-20">
-          <UIcon v-for="icon in page.logos.icons" :key="icon" :name="icon" class="w-12 h-12 flex-shrink-0 text-gray-500 dark:text-gray-400" />
-        </ULandingLogos>
+    <ULandingSection>
+      <ULandingLogos>
+        <UIcon v-for="icon in page.logos.icons" :key="icon" :name="icon" class="w-12 h-12 flex-shrink-0 text-gray-500 dark:text-gray-400" />
+      </ULandingLogos>
+    </ULandingSection>
 
-        <ULandingSection :title="page.faq.title" :description="page.faq.description">
-          <ULandingFAQ :items="page.faq.items" multiple default-open />
-        </ULandingSection>
-      </UPageBody>
-    </UPage>
-  </UContainer>
+    <ULandingSection :title="page.faq.title" :description="page.faq.description">
+      <ULandingFAQ :items="page.faq.items" multiple default-open class="max-w-4xl mx-auto" />
+    </ULandingSection>
+  </div>
 </template>
