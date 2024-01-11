494f3e3d68e239f87e33b8b356da2278541bcdf2
Benjamin Canac <canacb1@gmail.com>
2024-01-11 15:26:34 +0100

    chore: define global components with hook instead of plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,14 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
     'nuxt-og-image',
   ],
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter((c) => ['UButton'].includes(c.pascalName))
+
+      globals.forEach((c) => c.global = true)
+    }
+  },
   ui: {
     icons: ['heroicons', 'simple-icons']
   },
