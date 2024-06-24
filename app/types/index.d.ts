d64d3661a6b3dd5937f630803ffb3eefa0acb1a3
Benjamin Canac <canacb1@gmail.com>
2024-06-24 12:14:02 +0200

    chore(types): import from `@nuxt/content` over `@nuxt/content/dist/runtime/types`

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -1,4 +1,4 @@
-import type { ParsedContent } from '@nuxt/content/dist/runtime/types'
+import type { ParsedContent } from '@nuxt/content'
 
 export interface BlogPost extends ParsedContent {
   title: string
