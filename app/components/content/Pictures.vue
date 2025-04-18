ebb23cedbb2e858424df021f46e3e66d2a6853e2
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-04-18 12:00:47 +0200

    up

---
+++ app/components/content/Pictures.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="flex space-x-4 items-center justify-between"
+    :class="orientation === 'vertical' ? 'flex-col' : 'flex-row'"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  orientation: 'horizontal' | 'vertical'
+}>()
+</script>
