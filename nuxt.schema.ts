a84d22a2131ed27cd5bc0afbbc76efe56469de1f
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-04-22 16:35:15 +0200

    fix(schema): update app config schema for Studio

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -7,6 +7,29 @@ export default defineNuxtSchema({
       description: 'UI Customization.',
       icon: 'i-mdi-palette-outline',
       fields: {
+        colors: group({
+          title: 'Colors',
+          description: 'Manage main colors of your application',
+          icon: 'i-mdi-palette-outline',
+          fields: {
+            primary: field({
+              type: 'string',
+              title: 'Primary',
+              description: 'Primary color of your UI.',
+              icon: 'i-mdi-palette-outline',
+              default: 'green',
+              required: ['red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose']
+            }),
+            neutral: field({
+              type: 'string',
+              title: 'Neutral',
+              description: 'Neutral color of your UI.',
+              icon: 'i-mdi-palette-outline',
+              default: 'slate',
+              required: ['slate', 'gray', 'zinc', 'neutral', 'stone']
+            })
+          }
+        }),
         icons: group({
           title: 'Icons',
           description: 'Manage icons used in UI Pro.',
@@ -55,24 +78,6 @@ export default defineNuxtSchema({
               default: 'i-lucide-hash'
             })
           }
-        }),
-        colors: group({
-          primary: field({
-            type: 'string',
-            title: 'Primary',
-            description: 'Primary color of your UI.',
-            icon: 'i-mdi-palette-outline',
-            default: 'green',
-            required: ['red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose']
-          }),
-          neutral: field({
-            type: 'string',
-            title: 'Neutral',
-            description: 'Neutral color of your UI.',
-            icon: 'i-mdi-palette-outline',
-            default: 'slate',
-            required: ['slate', 'gray', 'zinc', 'neutral', 'stone']
-          })
         })
       }
     })
