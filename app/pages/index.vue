0dc1ba0ed2f4ec7f9ce5359ab8b6aafca68fa57f
Benjamin Canac <canacb1@gmail.com>
2025-04-25 11:42:51 +0200

    fix(seo): put back empty `titleTemplate` on index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,6 +5,7 @@ const title = page.value?.seo?.title || page.value?.title
 const description = page.value?.seo?.description || page.value?.description
 
 useSeoMeta({
+  titleTemplate: '',
   title,
   ogTitle: title,
   description,
